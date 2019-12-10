e191a89ff25359723837d553e357f22ea349191b
Auz <oo7marcusaurelius@gmail.com>
2019-12-10 10:06:13 +0530

    router created,views created,html file served

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const bodyparser=require('body-parser');
 const express=require('express');
-const adminroutes=require('./routes/admin');
-const shoproutes=require('./routes/shop');
+const path=require('path');
 
 const app=express();
 const PORT=3000;
 
+const adminroutes=require('./routes/admin');
+const shoproutes=require('./routes/shop');
+
 app.use(bodyparser.urlencoded({extended:true}));
-app.use(adminroutes);
-app.use(shoproutes);
+app.use('/admin'.adminroutes);
+app.use('',shoproutes);
+
+//if nothing runs then this middleware runs and shows and error
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'../','views','404.html'));
+});
+
 
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
